8be7eb0260f7436b556a442d23afdf426f836c04
Tetsu-is <hap30305@kwansei.ac.jp>
2025-01-18 03:05:09 +0900

    一旦jsでデプロイまで

--- functions/likes/[id].js
+++ functions/likes/[id].js
@@ -1,10 +1,10 @@
-import { type D1Database, type PagesFunction } from "@cloudflare/workers-types";
 
-export interface Env {
-  // If you set another name in wrangler.toml as the value for 'binding',
-  // replace "DB" with the variable name you defined.
-  DB: D1Database;
-}
+
+// export interface Env {
+//   // If you set another name in wrangler.toml as the value for 'binding',
+//   // replace "DB" with the variable name you defined.
+//   DB: D1Database;
+// }
 
 
 const corsHeaders = {
@@ -13,7 +13,7 @@ const corsHeaders = {
       "Access-Control-Max-Age": "86400",
 }
 
-export const onRequest: PagesFunction<Env> = async (context) => {
+export const onRequest = async (context) => {
     const id = context.params.id
     const q = "UPDATE blogs SET likes_count = likes_count + 1 WHERE id = ?"
     const result  = await context.env.DB.prepare(q)
