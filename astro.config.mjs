885a05e59e27ff8ad071e5e45a7cdbd10a9173c2
Tetsu-is <hap30305@kwansei.ac.jp>
2025-02-10 23:41:42 +0900

    use experimental server island for likes count

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import mdx from "@astrojs/mdx";
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
 	integrations: [mdx(), sitemap()],
+	adapter: cloudflare(),
+	output: 'hybrid',
+	experimental: {
+		serverIslands: true,
+	}
 });
