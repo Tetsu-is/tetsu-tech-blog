f0cc39d6f4bde2e9fdeafe7e5e8700e3868cf0f2
Tetsu-is <hap30305@kwansei.ac.jp>
2025-02-10 22:39:24 +0900

    check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
+import mdx from "@astrojs/mdx";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: "https://example.com",
 	integrations: [mdx(), sitemap()],
 });
