b60cf58ec6ef7c9c40dfbaf0fa8d0904e8dee9f8
Tetsu-is <hap30305@kwansei.ac.jp>
2025-02-10 23:07:39 +0900

    temp

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
+export type BlogSchema = {
+	title: string;
+	description: string;
+	pubDate: Date;
+	updatedDate?: Date;
+	heroImage?: string;
+	blogID: string;
+};
+
 const blog = defineCollection({
 	type: "content",
 	// Type-check frontmatter using a schema
