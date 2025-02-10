2c378bdc95a96cc23872a6fd8831baf0e71a7cd1
Tetsu-is <hap30305@kwansei.ac.jp>
2025-02-10 23:43:49 +0900

    linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,16 @@ import mdx from "@astrojs/mdx";
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare(),
-	output: 'hybrid',
+	output: "hybrid",
 	experimental: {
 		serverIslands: true,
-	}
+	},
 });
