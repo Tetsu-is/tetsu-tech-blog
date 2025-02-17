c9efe5793fabac9371646cfb97d717030d825a62
Tetsu-is <hap30305@kwansei.ac.jp>
2025-02-18 03:13:18 +0900

    temp

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://tetsu-tech-blog.pages.dev",
-	integrations: [mdx(), sitemap()],
+    site: "https://tetsu-tech-blog.pages.dev",
+    integrations: [mdx(), sitemap(), svelte()],
 });
---
+++ src/components/LikesContainer.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { writable } from "svelte/store";
+
+  const likes = writable(0);
+
+  function incrementLikes() {
+    likes.update((n) => n + 1);
+  }
+</script>
+
+<div class="likes-container">
+  <div class="likes-count">{$likes}</div>
+  <button class="like-button" on:click={incrementLikes}>Like</button>
+</div>
