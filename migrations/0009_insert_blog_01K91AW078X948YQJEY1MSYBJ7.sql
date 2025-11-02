05e27d99079364052341c135e4c5e70f1d3ec613
Tetsu-is <hap30305@kwansei.ac.jp>
2025-11-02 12:57:53 +0900

    fix font size

---
+++ migrations/0009_insert_blog_01K91AW078X948YQJEY1MSYBJ7.sql
@@ -0,0 +1,2 @@
+-- Migration number: 0009 	 2025-11-02T03:48:12.095Z
+INSERT INTO blogs (id, name, likes_count) VALUES ("01K91AW078X948YQJEY1MSYBJ7", "hogehoge", 0);
---
+++ src/content/blog/hogehoge.md
@@ -0,0 +1,7 @@
+---
+title: "hogehoge"
+description: "description"
+pubDate: "Nov 02 2025"
+heroImage: "/og/hogehoge.png"
+blogID: "01K91AW078X948YQJEY1MSYBJ7"
+---
